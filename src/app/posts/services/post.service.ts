fc9e944574cfa1695d119fd6551edee59852a911
bAmrish <bamrish@gmail.com>
2021-01-19 08:48:59 +0530

    Error handling using multiple catchError operators

--- src/app/posts/services/post.service.ts
+++ src/app/posts/services/post.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { tap } from "rxjs/operators";
+import { Observable, of, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 
 import { Post } from "../models/post.model";
 
@@ -10,6 +10,21 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>("https://jsonplaceholder.typicode.com/todos");
+    return this.http
+      .get<Post[]>("https://jsonplaceholder.typicode.com/todoss")
+      .pipe(
+        //Order is important
+        // if I swap the first catch error fucntion with the second one
+        // the second in the list will never be called because in that case
+        // the first one is not thrwoing error but returing just an empty observable.
+        catchError(error => {
+          console.log("Error caught in catchError");
+          return throwError(error);
+        }),
+        catchError(error => {
+          console.log("Error caught in catchError2", error);
+          return of([]);
+        })
+      );
   }
 }
